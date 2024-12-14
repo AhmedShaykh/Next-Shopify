3363c8460d329a0864d2d506b21bb93c717df9b1
AhmedShaykh <98047091+AhmedShaykh@users.noreply.github.com>
2024-12-14 20:22:01 +0500

    Final Commit

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -57,11 +57,11 @@ export default async function Footer() {
           </p>
           <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
           <p>
-            <a href="https://github.com/vercel/commerce">View the source</a>
+            <a href="https://github.com/AhmedShaykh/Next-Shopify-Headless">View the source</a>
           </p>
           <p className="md:ml-auto">
-            <a href="https://vercel.com" className="text-black dark:text-white">
-              Created by ▲ Vercel
+            <a href="https://github.com/AhmedShaykh" className="text-black dark:text-white">
+              Created by Ahmed Saleem Shaikh
             </a>
           </p>
         </div>
